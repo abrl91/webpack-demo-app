9aef4bffa0599212336828b47c1a9be508731a12
Amit Barletz <amitbarletz@Amits-MBP.lan>
2020-09-27 22:06:00 +0300

    first commit

---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const common = require('./webpack.config');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const  MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        filename: "[name].[contentHash].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserPlugin(),
+            new HtmlWebpackPlugin(
+                {
+                    template: "./src/template.html", 
+                    minify: {
+                        removeComments: true,
+                        removeAttributeQuotes: true,
+                        collapseWhitespace: true
+                    }
+                })
+        ]
+    },
+   plugins: [
+       new MiniCssExtractPlugin({filename: "[name].[contentHash].css"}), 
+       new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', "sass-loader"]
+            }
+        ]
+    }
+});
